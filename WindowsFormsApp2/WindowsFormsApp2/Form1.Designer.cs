8c1eb37199c72faa4a6dcf04810d736f76b19bd6
ahmedfathiabdelslaam <91540327+ahmedfathiabdelslaam@users.noreply.github.com>
2022-05-24 09:44:54 +0200

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.empoloyeesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.accountingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inventoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addPaitiontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fToolStripMenuItem,
+            this.empoloyeesToolStripMenuItem,
+            this.accountingToolStripMenuItem,
+            this.inventoryToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fToolStripMenuItem
+            // 
+            this.fToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addPaitiontToolStripMenuItem});
+            this.fToolStripMenuItem.Name = "fToolStripMenuItem";
+            this.fToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fToolStripMenuItem.Text = "file";
+            // 
+            // empoloyeesToolStripMenuItem
+            // 
+            this.empoloyeesToolStripMenuItem.Name = "empoloyeesToolStripMenuItem";
+            this.empoloyeesToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.empoloyeesToolStripMenuItem.Text = "empoloyees";
+            // 
+            // accountingToolStripMenuItem
+            // 
+            this.accountingToolStripMenuItem.Name = "accountingToolStripMenuItem";
+            this.accountingToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.accountingToolStripMenuItem.Text = "accounting";
+            // 
+            // inventoryToolStripMenuItem
+            // 
+            this.inventoryToolStripMenuItem.Name = "inventoryToolStripMenuItem";
+            this.inventoryToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.inventoryToolStripMenuItem.Text = "inventory";
+            // 
+            // addPaitiontToolStripMenuItem
+            // 
+            this.addPaitiontToolStripMenuItem.Name = "addPaitiontToolStripMenuItem";
+            this.addPaitiontToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addPaitiontToolStripMenuItem.Text = "add patient";
+            this.addPaitiontToolStripMenuItem.Click += new System.EventHandler(this.addPaitiontToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(75, 17);
+            this.toolStripStatusLabel1.Text = "Ahmed Fathi";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Value = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hospital System";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addPaitiontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem empoloyeesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem accountingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inventoryToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Patient Form.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Patient_Form : Form
+    {
+        HospitalDBEntities db = new HospitalDBEntities();
+        Paitent _paitent = null;
+        public Patient_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Paitent.ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            db.Paitent.Add(new Paitent()
+            {
+                Name = textBox2.Text,
+                Gender = Male.Checked || false,
+                BirthDate = dateTimePicker1.Value,
+                Smoking = checkBox1.Checked,
+                IsFat = checkBox2.Checked 
+            });
+            db.SaveChanges();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(textBox1.Text);
+            _paitent = db.Paitent.First(p1 => p1.ID == id);
+            textBox2.Text = _paitent.Name;
+            if(_paitent.Gender == true)
+            {
+                Male.Checked = true;
+            }
+            else
+             {
+                Female.Checked = true;
+             }
+            dateTimePicker1.Value = Convert.ToDateTime(_paitent.BirthDate );
+            checkBox1.Checked = Convert.ToBoolean(_paitent.Smoking );
+            checkBox2.Checked = Convert.ToBoolean(_paitent.IsFat);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            _paitent.Name = textBox2.Text;
+            _paitent.BirthDate = dateTimePicker1.Value;
+            db.SaveChanges();
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            db.Paitent.Remove(_paitent);
+            db.SaveChanges();
+        }
+    }
+}
